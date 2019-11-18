8f404dc4bb80dae2103d9f9ec0ece599f7f0e80e
Venkatesh Chellappa <genomics@live.com>
2019-11-18 14:45:57 +0100

    fixed conflicts from previous commits -- missing lines from previous merge of venki_devtest with develop branch

--- autoseq/pipeline/liqbio.py
+++ autoseq/pipeline/liqbio.py
@@ -203,6 +203,7 @@ class LiqBioPipeline(ClinseqPipeline):
         svaba = Svaba()
         svaba.input_normal = normal_bam
         svaba.input_tumor = cancer_bam
+        svaba.scratch = self.scratch
         svaba.reference_sequence = self.refdata["bwaIndex"]
         svaba.threads = self.maxcores
         svaba.target_bed = self.refdata['targets'][target_name]['targets-bed-slopped20-gz']
@@ -238,6 +239,7 @@ class LiqBioPipeline(ClinseqPipeline):
 
 
         lumpy = Lumpy()
+        lumpy.scratch = self.scratch
         lumpy.input_normal = normal_bam
         lumpy.input_tumor = cancer_bam
         lumpy.normal_discordants = "{}/svs/lumpy/{}-discordants.bam".format(self.outdir, normal_capture_str)
@@ -412,6 +414,7 @@ class LiqBioPipeline(ClinseqPipeline):
         call_consensus_reads = CallDuplexConsensusReads()
         call_consensus_reads.input_bam = group_reads.output_bam
         call_consensus_reads.scratch = self.scratch
+        call_consensus_reads.threads = self.maxcores
         call_consensus_reads.output_bam = "{}/bams/{}/{}.consensus.bam".format(self.outdir, capture_kit, clinseq_barcode)
         call_consensus_reads.jobname = "call-duplex-consensus-reads" + '-' + clinseq_barcode
         self.add(call_consensus_reads)
